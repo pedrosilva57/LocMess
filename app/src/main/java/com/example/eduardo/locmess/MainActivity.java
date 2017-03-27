72f84c9f111d35e0658a37aae111da0720f38d54
Eduardo <eduardodel@gmail.com>
2017-03-27 09:14:18 +0100

    New version

--- app/src/main/java/com/example/eduardo/locmess/MainActivity.java
+++ app/src/main/java/com/example/eduardo/locmess/MainActivity.java
@@ -1,17 +1,20 @@
 package com.example.eduardo.locmess;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.ListView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-    //private ListView mTextView;
+    ActionBar actionBar;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -20,13 +23,16 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_location:
-                    mTextMessage.setText(R.string.locat);
+                    actionBar.setTitle("Location");
+                    setLocationFragment();
                     return true;
                 case R.id.navigation_messages:
-                    mTextMessage.setText(R.string.messages);
+                    actionBar.setTitle("Messages");
+                    setMessagesFragment();
                     return true;
                 case R.id.navigation_profile:
-                    mTextMessage.setText(R.string.profile);
+                    actionBar.setTitle("Profile");
+                    setProfileFragment();
                     return true;
             }
             return false;
@@ -38,11 +44,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
-        //mTextView = (ListView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        actionBar=getSupportActionBar();
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigationBar);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
     }
 
+    public void setLocationFragment(){
+        FragmentManager fm= getSupportFragmentManager();
+        FragmentTransaction ft=fm.beginTransaction();
+        Fragment locationFragment=LocationFragment.newInstance();
+        ft.replace(R.id.locationListView,locationFragment);
+        ft.commit();
+    }
+
+    public void setMessagesFragment(){
+        FragmentManager fm= getSupportFragmentManager();
+        FragmentTransaction ft=fm.beginTransaction();
+        Fragment messagesFragment=MessagesFragment.newInstance();
+        ft.replace(R.id.messagesListView,messagesFragment);
+        ft.commit();
+    }
+
+    public void setProfileFragment(){
+        FragmentManager fm= getSupportFragmentManager();
+        FragmentTransaction ft=fm.beginTransaction();
+        Fragment profileFragment=ProfileFragment.newInstance();
+        ft.replace(R.id.content_id,profileFragment);
+        ft.commit();
+    }
 }
