9b1357dc3d83af9db02d46be4222aa4483b5a656
Eduardo <eduardodel@gmail.com>
2017-04-19 12:18:34 +0100

    New version

--- app/src/main/java/com/example/eduardo/locmess/AddLocationActivity.java
+++ app/src/main/java/com/example/eduardo/locmess/AddLocationActivity.java
@@ -1,48 +1,54 @@
 package com.example.eduardo.locmess;
 
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+public class AddLocationActivity extends AppCompatActivity {
 
-public class AddLocationActivity extends FragmentActivity implements OnMapReadyCallback {
-
-    private GoogleMap mMap;
+    double longitude;
+    double latitude;
+    private Button b_get;
+    private TrackGPS gps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_location);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        b_get = (Button)findViewById(R.id.locatAdd);
 
+
+
+        b_get.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                gps = new TrackGPS(AddLocationActivity.this);
+
+
+                if(gps.canGetLocation()){
+
+
+                    longitude = gps.getLongitude();
+                    latitude = gps .getLatitude();
+
+                    Toast.makeText(getApplicationContext(),"Longitude:"+Double.toString(longitude)+"\nLatitude:"+Double.toString(latitude),Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+
+                    gps.showSettingsAlert();
+                }
+
+            }
+        });
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng arcoCego = new LatLng(38.736042, -9.142222);
-        mMap.addMarker(new MarkerOptions().position(arcoCego).title("Marker in Arco do Cego"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(arcoCego));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+    protected void onDestroy() {
+        super.onDestroy();
+        gps.stopUsingGPS();
     }
 }
