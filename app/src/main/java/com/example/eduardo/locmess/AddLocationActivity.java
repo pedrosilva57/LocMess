900a8cf3ac47e2ede2902a7fda3c764c881a3ad2
Eduadel <eduardodel@gmail.com>
2017-04-19 18:18:25 +0100

    vw

--- app/src/main/java/com/example/eduardo/locmess/AddLocationActivity.java
+++ app/src/main/java/com/example/eduardo/locmess/AddLocationActivity.java
@@ -1,7 +1,11 @@
 package com.example.eduardo.locmess;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -10,16 +14,16 @@ public class AddLocationActivity extends AppCompatActivity {
 
     double longitude;
     double latitude;
-    Button b_get;
     private TrackGPS gps;
+    ActionBar ab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_location);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         gps = new TrackGPS(AddLocationActivity.this);
-
 
         if(gps.canGetLocation()){
 
@@ -30,8 +34,22 @@ public class AddLocationActivity extends AppCompatActivity {
         }
         else
         {
-
             gps.showSettingsAlert();
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 
